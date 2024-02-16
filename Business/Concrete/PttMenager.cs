b1bd9502a1d9c91601ee81ae9db97e1bb6a41a50
erkan <erkan@OZGUN>
2024-02-16 20:42:04 +0300

    Add project files.

---
+++ Business/Concrete/PttMenager.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttMenager:ISupplierService
+    {
+        
+        private IApplicantService _applicantservice;
+
+        public PttMenager(IApplicantService applicantService)// Constructor new yapıldığında çalışır
+        {
+            _applicantservice = applicantService;
+        }
+            
+
+        public void GiveMask(Person person)
+        {
+            
+            if(_applicantservice.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " için maske verildi");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName +" için maske VERİLEMEDİ");
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Business.Concrete;
+using Entities.Concrete;
+
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Person person1 = new Person();
+            person1.FirstName = "Erkan";
+            person1.LastName = "Özgün";
+            person1.DateOfBirthYear = 2002;
+            person1.NationalIdentity = 123;
+
+            PttMenager pttmenager = new PttMenager(new PersonMenager());
+            pttmenager.GiveMask(person1);
+
+
+
+            Console.ReadLine();
+        }
+        
+ 
+
+  
+    }
+
+ 
+}
